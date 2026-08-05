570e86b1223ac81ddc3b53d459fba64b0611cdb2
sayedmujeeb12-coder <sayedmujeeb12@gmail.com>
2026-08-05 01:37:38 -0400

    Cart.h

--- Cart.h
+++ Cart.h
@@ -2,28 +2,42 @@
 #define CART_H
 
 #include "Course.h"
-#include <string> // Include the string header for using std::string
+#include <list> // STL list to store courses
+
+using namespace std;
 
 class Cart
 {
 private:
-    Course* courses;
-    std::string* schedules;
-    int totalCourses;
-    double subtotal;
+    list<Course> courses;   // List to store all courses in the cart
+    double subtotal;        // Total price before tax
 
 public:
+    // Default constructor
     Cart();
-    ~Cart();
 
+    // Add a course to the cart
     void addCourse(const Course& course);
+
+    // Display all courses in the cart
     void listCourses() const;
+
+    // Remove all courses from the cart
     void clear();
 
+    // Check if the cart is empty
     bool isEmpty() const;
+
+    // Return the number of courses in the cart
     int getTotalCourses() const;
+
+    // Return the subtotal
     double getSubtotal() const;
+
+    // Calculate 13% tax
     double getTax() const;
+
+    // Return the total price including tax
     double getTotalWithTax() const;
 };
 
